e61988178ceaec972842a2a2c97cbf73ddd460e9
elnascimento <evandroln@gmail.com>
2017-06-02 23:27:03 -0300

    Teste

---
+++ CursoMarcosUdemy/teste_sintaxe.py
@@ -0,0 +1,13 @@
+'''
+Comentário de várias linhas
+no python
+'''
+
+#Como saber qual o tipo de uma variável
+inteiro = 8
+pontoflutuante = 2.5
+nome = 'evandro'
+
+print(type(inteiro))
+print(type(pontoflutuante))
+print(type(nome))
